6df25644f2bccbd6562afe192017e748dfd10531
Jorge Bugarini <l21550317@chihuahua2.tecnm.mx>
2024-05-13 08:51:39 -0600

    Estructura basica del proyecto implementada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { MainPageComponent } from './shared/pages/main-page/main-page.component';
+import { AdminPageComponent } from './shared/pages/admin-page/admin-page.component';
+import { CatalogPageComponent } from './shared/pages/catalog-page/catalog-page.component';
+import { ProductPageComponent } from './shared/pages/product-page/product-page.component';
+import { AddressesPageComponent } from './shared/pages/addresses-page/addresses-page.component';
+import { OrderStatusPageComponent } from './shared/pages/order-status-page/order-status-page.component';
+import { CartPageComponent } from './shared/pages/cart-page/cart-page.component';
+import { OrderHistoryPageComponent } from './shared/pages/order-history-page/order-history-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    MainPageComponent,
+    AdminPageComponent,
+    CatalogPageComponent,
+    ProductPageComponent,
+    AddressesPageComponent,
+    OrderStatusPageComponent,
+    CartPageComponent,
+    OrderHistoryPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/pages/addresses-page/addresses-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addresses-page',
+  templateUrl: './addresses-page.component.html',
+  styleUrl: './addresses-page.component.css'
+})
+export class AddressesPageComponent {
+
+}
