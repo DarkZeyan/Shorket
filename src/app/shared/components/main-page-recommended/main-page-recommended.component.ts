ed529807fb31bf4530e909055d422b6c72955872
DarkZeyan <esonic0234@hotmail.com>
2024-05-16 01:41:04 -0600

    Añadido componente para los articulos recomendados

--- src/app/shared/components/main-page-recommended/main-page-recommended.component.ts
+++ src/app/shared/components/main-page-recommended/main-page-recommended.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-main-page-recommended',
+  selector: 'main-page-recommended',
   templateUrl: './main-page-recommended.component.html',
-  styleUrls: ['./main-page-recommended.component.scss'],
+  styleUrls: ['./main-page-recommended.component.css'],
 })
-export class MainPageRecommendedComponent  implements OnInit {
+export class MainPageRecommendedComponent{
 
   constructor() { }
-
-  ngOnInit() {}
 
 }
