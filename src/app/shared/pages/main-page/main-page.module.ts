98b5b8a736308be11c80aa21f9a25fd4d02dcfc8
DarkZeyan <esonic0234@hotmail.com>
2024-05-15 22:42:11 -0600

    Implementacion del componente de Slider de la pagina principal

--- src/app/shared/pages/main-page/main-page.module.ts
+++ src/app/shared/pages/main-page/main-page.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { MainPageSliderComponent } from '../../components/main-page-slider/main-page-slider.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, NgbCarouselModule],
   exports: [MainPageSliderComponent],
   declarations: [MainPageSliderComponent],
   providers: [],
