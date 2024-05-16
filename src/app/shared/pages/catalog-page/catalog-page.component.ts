0b3a145fbd20ff72b55a9e49e19505cf04202332
DarkZeyan <esonic0234@hotmail.com>
2024-05-16 15:40:57 -0600

    Modificacion a navbar para la pagina de categorias

--- src/app/shared/pages/catalog-page/catalog-page.component.ts
+++ src/app/shared/pages/catalog-page/catalog-page.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { Category } from '@shared/intefaces/category.interface';
 @Component({
   selector: 'app-catalog-page',
   templateUrl: './catalog-page.component.html',
   styleUrl: './catalog-page.component.css'
 })
 export class CatalogPageComponent {
+
+  public categories:Category[] = [
+    { id:1, name: 'Computadoras', image: '' },
+    { id:2, name: 'Celulares', image: '' },
+    { id:3, name: 'Smartwatches', image: '' },
+    { id:4, name: 'Perifericos', image: '' },
+    { id:5, name: 'Audio', image: '' },
+    { id:6, name: 'Hardware', image: '' },
+  ]
+
+  constructor() {
+  }
+
 
 }
