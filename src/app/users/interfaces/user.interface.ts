24c7841882cccd328289fc79dc67e1bc43b54485
DarkZeyan <esonic0234@hotmail.com>
2024-05-18 02:11:25 -0600

    Modulo de usuarios, interfaz y servicio provisional implementados.

---
+++ src/app/users/interfaces/user.interface.ts
@@ -0,0 +1,13 @@
+export interface User{
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  last_name: string;
+  birth_date: string;
+}
+export interface AdminUser{
+  id: number;
+  username: string;
+  password: string;
+}
