95f51db51d0d319df43c73fc571b7be5eab16529
DarkZeyan <esonic0234@hotmail.com>
2024-05-18 19:28:16 -0600

    Procesamiento del carrito

--- src/app/shared/pages/cart-page/cart-page.component.ts
+++ src/app/shared/pages/cart-page/cart-page.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CartItem } from '@shared/intefaces/cart.interface';
+import { CartService } from './services/cart.service';
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
   styleUrl: './cart-page.component.css'
 })
-export class CartPageComponent {
+export class CartPageComponent implements OnInit {
+
+  cartItems: CartItem[] = [];
+
+
+
+  constructor(private cartService:CartService) {};
+
+  ngOnInit() {
+    this.cartItems = this.cartService.cartItems;
+  }
+
+
+
+
 
 }
--- src/app/shared/pages/cart-page/components/table-component/table-component.component.ts
+++ src/app/shared/pages/cart-page/components/table-component/table-component.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CartItem } from '@shared/intefaces/cart.interface';
+import { CartService } from '../../services/cart.service';
 
 @Component({
-  selector: 'app-table-component',
+  selector: 'cart-table-component',
   templateUrl: './table-component.component.html',
   styleUrl: './table-component.component.css'
 })
-export class TableComponentComponent {
+export class TableComponentComponent implements OnInit{
+  @Input()
+  cartItems: CartItem[] = [];
+  total:number = 0;
+  constructor(private cartService:CartService) { }
 
+  ngOnInit() {
+    this.cartItems = this.cartService.cartItems;
+    this.total = this.getCartTotal(this.cartItems);
+  }
+  getCartTotal(cartItems: CartItem[]) {
+    return this.cartService.getTotalPrice(cartItems);
+  }
 }
