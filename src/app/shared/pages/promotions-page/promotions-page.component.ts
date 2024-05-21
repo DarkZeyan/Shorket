1caa3502b46adcf3a3c6584640348652ab04fa0d
DarkZeyan <esonic0234@hotmail.com>
2024-05-21 12:34:32 -0600

    Template de codigos promocionales terminada

--- src/app/shared/pages/promotions-page/promotions-page.component.ts
+++ src/app/shared/pages/promotions-page/promotions-page.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Promotion } from '@shared/intefaces/promotion.interface';
+import { PromotionService } from '../../services/promotion.service';
 
 @Component({
   selector: 'app-promotions-page',
   templateUrl: './promotions-page.component.html',
   styleUrl: './promotions-page.component.css'
 })
-export class PromotionsPageComponent {
+export class PromotionsPageComponent implements OnInit {
+
+  promotions: Promotion[] = [];
+
+  constructor(private promotionService: PromotionService) { }
+  ngOnInit() {
+    this.promotions = this.promotionService.getPromotions();
+  }
+
+
 
 }
