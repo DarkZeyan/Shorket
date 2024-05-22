5cd309e8c7abafcb3ba41d007e4938708025f170
DarkZeyan <esonic0234@hotmail.com>
2024-05-22 04:59:23 -0600

    Template de login form terminado

--- src/app/users/pages/login-page/login-page.component.ts
+++ src/app/users/pages/login-page/login-page.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { Modal } from 'bootstrap';
+import { last } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements AfterViewInit {
 
+  modal!: Modal;
+  loginForm: FormGroup;
+  registerForm: FormGroup;
+  datePickerModel!: NgbDateStruct;
+
+  // Controladores de alerts
+  loginFailed: boolean = false;
+  registerFailed: boolean = false;
+  registerSuccess: boolean = false;
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
+      // Define form controls for each login field
+      email_login: ['', Validators.required],
+      password_login: ['', Validators.required]
+    });
+
+    this.registerForm = this.fb.group({
+      // Define form controls for each register field
+      name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      verify_password: ['', Validators.required],
+      birth_date: ['', Validators.required]
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.modal = new Modal(document.getElementById('registerNewUser')!);
+  }
+
+  onSubmit(): void {
+    console.log('Entro')
+    if (this.loginForm.valid) {
+      // Call the login service
+      console.log(this.loginForm.value);
+      this.loginForm.reset();
+      // this.router.navigate(['/home']);
+    }
+  }
+  onSubmitRegister(): void {
+    if (this.registerForm.valid) {
+      // Call the register service
+      this.registerForm.reset();
+      this.modal.hide();
+      this.registerSuccess = true;
+      setInterval(() => {
+        this.registerSuccess = false;
+      }, 8000);
+    }
+  }
 }
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 
 const routes = [
   { path: 'login', component: LoginPageComponent }
@@ -13,6 +15,8 @@ const routes = [
     LoginPageComponent
   ],
   imports: [
+    ReactiveFormsModule,
+    NgbDatepickerModule,
     CommonModule,
     BrowserModule,
     RouterModule.forChild(routes)
