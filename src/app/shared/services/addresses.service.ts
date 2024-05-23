eef9d71ccc9a73e50d49bb947945fcf0f319295d
Jorge Bugarini <l21550317@chihuahua2.tecnm.mx>
2024-05-22 18:12:43 -0600

    Cambios para direcciones

--- src/app/shared/services/addresses.service.ts
+++ src/app/shared/services/addresses.service.ts
@@ -1,15 +1,39 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Address } from '@shared/intefaces/address.interface';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class AddressService {
 
-  private addresses:Address[] = [];
+  private API_URL = 'http://localhost:8000/addresses';
 
-  constructor() { }
+  private addresses: BehaviorSubject<Address[]> = new BehaviorSubject<Address[]>([]);
+  private addressesLoaded: boolean = false;
 
-  getAddressesByUser(userId: number) {
+  constructor(private httpClient: HttpClient) { }
 
+  getAddressesByUser(user_id: number): Observable<Address[]> {
+
+    if (!this.addressesLoaded) {
+      return this.httpClient.get<Address[]>(`${this.API_URL}/user/${user_id}`).pipe(
+        tap(addresses => {
+          this.addresses.next(addresses);
+        })
+      );
+    } else {
+      return this.addresses.asObservable();
+    }
   }
+
+  getAddressById(id: number): Observable<Address> {
+    return this.httpClient.get<Address>(`${this.API_URL}/${id}`);
+  }
+
+
+  deleteAddress(address_id: number): Observable<any> {
+    return this.httpClient.delete(`${this.API_URL}/${address_id}`);
+  }
+
 
 }
